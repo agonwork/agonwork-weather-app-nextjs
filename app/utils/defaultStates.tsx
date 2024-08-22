f8f0e6157b99941397280f67996084f254351ca9
agon2002 <ac52981@ubt-uni.net>
2024-08-22 23:45:44 +0200

    Adding project to Github

---
+++ app/utils/defaultStates.tsx
@@ -0,0 +1,39 @@
+const defaultStates = [
+  {
+    name: "Prishtina",
+    country: "XK",
+    state: "Kosovo",
+    lat: 42.667542,
+    lon: 21.166191,
+  },
+  {
+    name: "London",
+    country: "GB",
+    state: "England",
+    lat: 51.5074,
+    lon: 0.1278,
+  },
+  {
+    name: "New York",
+    country: "US",
+    state: "New York",
+    lat: 40.7128,
+    lon: -74.006,
+  },
+  {
+    name: "Paris",
+    country: "FR",
+    state: "France",
+    lat: 48.8575,
+    lon: 2.3514,
+  },
+  {
+    name: "Barcelona",
+    country: "ES",
+    state: "Catalonia",
+    lat: 41.3828939,
+    lon: 2.1774322,
+  },
+];
+
+export default defaultStates;
